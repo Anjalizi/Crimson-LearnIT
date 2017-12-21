f80c56f9ad857dee8c3ab0c3ecf21d3bac8c587b
Anjali Jain <aj2966@gmail.com>
2017-12-22 02:12:40 +0530

    adding bg images, completed auth section

--- crims/settings.py
+++ crims/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'crims.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'None/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
