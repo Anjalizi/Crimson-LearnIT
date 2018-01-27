38c4f341f6ab4a070a305af9ba055113da283818
Anjali Jain <aj2966@gmail.com>
2018-01-28 00:53:27 +0530

    upload file, color histogram func

--- media_handling/color_kmeans.py
+++ media_handling/color_kmeans.py
@@ -26,3 +26,13 @@ image = image.reshape((image.shape[0] * image.shape[1],3))
 # cluster the pixel intensities
 clt = KMeans(n_clusters = args["clusters"])
 clt.fit(image)
+
+# build a histogram of clusters and then create a figure representing the number of pixels labeled to each color
+hist = utils.centroid_histogram(clt)
+bar = utils.plot_colors(hist, clt.cluster_centers_)
+
+# show our color bart
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
